77c95205ef13e60e5eaa471e6d70f12765ece3b0
Grigore <grigore.mereacre95@gmail.com>
2020-07-21 17:21:19 -0500

    impoerted components in app.js

--- employee-directory/src/App.js
+++ employee-directory/src/App.js
@@ -1,12 +1,24 @@
 import React from 'react';
+import { BrowserRouter as Router, Route} from 'react-router-dom';
+import Employees from './pages/Employees';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Wrapper from './components/Wrapper';
 
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <div>
+        <Navbar />
+        <Wrapper>
+          {/* <Route exact path="/" component={About} />
+          <Route exact path="/about" component={About} /> */}
+          <Route exact path="/all" component={Employees} />
+        </Wrapper>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
