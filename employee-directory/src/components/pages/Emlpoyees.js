4f8a005a490413d586f1bc154e14467ec983a1f3
Grigore <grigore.mereacre95@gmail.com>
2020-07-21 17:49:28 -0500

    eployee page all then functions and all the componenst imported

---
+++ employee-directory/src/components/pages/Emlpoyees.js
@@ -0,0 +1,96 @@
+import React, { Component } from "react";
+import Api from "../utils/Api";
+import Card from "../components/Card";
+import Row from "../components/Row";
+import Container from "../components/Container";
+import Column from "../components/Column";
+import "./styles.css";
+
+class Employees extends Component {
+  state = {
+    allResults: [],
+    allResultsParsed: [],
+    results: [],
+  };
+
+  componentDidMount() {
+    Api.findPeople().then((res) => {
+      this.setState({
+        allResults: res.data.results,
+        allResultsParsed: res.data.results,
+        results: res.data.result,
+      });
+    });
+  }
+
+  handleSearchFN = (e) => {
+    const queryFirstName = e.target.value;
+    this.sortPeopleFN(queryFirstName);
+  };
+  handleSearchLN = (e) => {
+    const queryLastName = e.target.value;
+    this.sortPeopleLN(queryLastName);
+  };
+  //Sorting employees by first name
+  sortPeopleFN = (value) => {
+    let searchResults = this.state.allResults.filter((x) => {
+      return x.name.first.toLowerCase().includes(value);
+    });
+    this.setState({
+      allResultsParsed: searchResults,
+    });
+  };
+  //Sorting employees bu last name
+  sortPeopleLN = (value) => {
+    let searchResults = this.state.allResults.filter((x) => {
+      return x.name.last.toLowerCase().includes(value);
+    });
+    this.setState({
+      allResultsParsed: searchResults,
+    });
+  };
+
+  render() {
+    const results = this.state.allResultsParsed;
+    return (
+      <div>
+        <h1 className="text-center">Corporation</h1>
+        <form className="search-form form-inline">
+          <div>
+            <input
+              className="employeeSearch"
+              placeholder="Search by first name"
+              onChange={this.handleSearchFN}
+            />
+            <input
+              className="employeeSearch"
+              placeholder="Search by last name"
+              onChange={this.handleSearchLN}
+            />
+          </div>
+        </form>
+        <Container>
+          <Row>
+            {results.map((employees, 1) => (
+              <Column key={i}>
+                <Card
+                  firstName={employees.name.first}
+                  lastName={employees.name.last}
+                  image={employees.picture.large}
+                  fullName={employees.name.first + employees.name.last}
+                  age={employees.dob.age}
+                  DOB={employees.dob.date}
+                  ssn={employeesid.value}
+                  email={employees.email}
+                  phoneNumber={employees.phone}
+                />
+              </Column>
+            ))}
+          </Row>
+        </Container>
+      </div>
+    );
+  }
+}
+
+export default Employees;
