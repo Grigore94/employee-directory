45bd51d0e0b587cef3a112f67ff109d6ac56dd8b
Grigore <grigore.mereacre95@gmail.com>
2020-07-21 21:09:37 -0500

    added styles to cards

--- employee-directory/src/components/Card/index.js
+++ employee-directory/src/components/Card/index.js
@@ -6,10 +6,12 @@ function Card (props) {
         <div className="img"> 
         <img className="card-img-top" src={props.image} alt={`Our employee ${props.firstname} ${props.lastName}`}></img>
         <div className="card-body">
-    <h4 className="card-title">{`${props.firstName}${props.lastName}`}</h4>
-    <p className="caard=text">Age:{props.age}</p>
-    <p className="caard=text">Phone:{props.phoneNumber}</p>
-    <p className="caard=text">SSN:{props.ssn}</p>
+    <h4 className="card-title">{`${props.firstName} ${props.lastName}`}</h4>
+    <p className="card-text">Age:{props.age}</p>
+    
+    <p className="card-text">Phone:{props.phoneNumber}</p>
+    <p className="card-text">Email:{props.email}</p>
+    <p className="card-text">SSN:{props.ssn}</p>
         </div>
         </div>
     )
