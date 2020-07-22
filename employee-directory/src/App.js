241178f4e34d617eae7944bd9daf88f960d9998a
Grigore <grigore.mereacre95@gmail.com>
2020-07-21 21:12:58 -0500

    added exact path to about component

--- employee-directory/src/App.js
+++ employee-directory/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route} from 'react-router-dom';
-import Employees from '/Users/grigore/Github/eployee-dir/employee-directory/employee-directory/src/pages/Emlpoyees.js';
+import Employees from './pages/Emlpoyees.js';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Wrapper from './components/Wrapper';
@@ -14,7 +14,7 @@ function App() {
         <Wrapper>
           {/* <Route exact path="/" component={About} />
           <Route exact path="/about" component={About} /> */}
-          <Route exact path="/employees" component={Employees} />
+          <Route exact path="/all" component={Employees} />
         </Wrapper>
         <Footer />
       </div>
