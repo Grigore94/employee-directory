bb814a31667f71c0089d8eb802f26aec721c337b
Grigore <grigore.mereacre95@gmail.com>
2020-07-21 22:35:57 -0500

    added backgroung img to about page

--- employee-directory/src/utils/Api.js
+++ employee-directory/src/utils/Api.js
@@ -3,6 +3,6 @@ import axios from "axios";
 // API random request to get 20 employees
 export default {
   findPeople: function() {
-      return axios.get('https://randomuser.me/api/?results=20&nat=us');
+      return axios.get('https://randomuser.me/api/?results=21&nat=us');
   }
 }
