2fc1585fdddd4d73620a5f458b467065dfa8d38d
Grigore <grigore.mereacre95@gmail.com>
2020-07-21 21:14:10 -0500

    Updates

---
+++ precache-manifest.89d59b8cbeb179c330b1fea83196fbf1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1f6856d7cde3f195f08ad0866fef162",
+    "url": "/employee-directory/index.html"
+  },
+  {
+    "revision": "d9ee6c70a49766db5bcd",
+    "url": "/employee-directory/static/css/main.bd275f3b.chunk.css"
+  },
+  {
+    "revision": "4d60b5203217e2cf1185",
+    "url": "/employee-directory/static/js/2.93e3abd1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/employee-directory/static/js/2.93e3abd1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9ee6c70a49766db5bcd",
+    "url": "/employee-directory/static/js/main.11fc2456.chunk.js"
+  },
+  {
+    "revision": "79d7c4f4ad2a4d373d4e",
+    "url": "/employee-directory/static/js/runtime-main.759497ab.js"
+  }
+]);
